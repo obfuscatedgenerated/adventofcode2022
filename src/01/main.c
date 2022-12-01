b8cee49efbbcbe206196b7f7a92f86746e9978c3
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-12-01 13:46:20 +0000

    completed code for 01

--- src/01/main.c
+++ src/01/main.c
@@ -2,45 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LEN 32 // maximum length of a string (line in the file)
+
 int main() {
-    FILE* handle;
-    char* buffer;
+    FILE *handle;
 
     handle = fopen("01.txt", "r");
     if (handle == NULL) {
-        printf("Error opening file");
+        fputs("Error opening file", stderr);
         return 1;
     }
-
-    fseek(handle, 0, SEEK_END);
-    size_t length = ftell(handle);
-    fseek(handle, 0, SEEK_SET);
-
-    buffer = malloc(length + 1);
-
-    if (buffer == NULL) {
-        printf("Error allocating memory");
-        return 1;
-    }
-
-    // reading line by line would actually simplify this a lot
-    // then i can just strtol or check for empty line
-    fread(buffer, 1, length, handle); // could read line-by-line, but this is easier
-    fclose(handle);
-
-    buffer[length] = '\0';
 
     int current_calories = 0;
-    int total_calories = 0;
     int record_calories = 0;
 
-    for (int i = 0; i < length; i++) {
-        if (buffer[i] == '\n') {
-            if (total_calories > record_calories) {
-                record_calories = total_calories;
+    // read the file line by line
+    char buffer[MAX_LEN];
+    while (fgets(buffer, MAX_LEN, handle) != NULL) {
+        // remove the newline character
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        if (strcmp(buffer, "") == 0) {
+            // new elf!
+
+            if (current_calories > record_calories) {
+                record_calories = current_calories;
             }
-            total_calories = 0;
+
+            current_calories = 0;
+
+            continue;
         }
+
+        // parse the line into a base10 integer
+        char *end_ptr;
+        int calories = strtol(buffer, &end_ptr, 10);
+
+        if (end_ptr == buffer || *end_ptr != '\0' || calories < 0) { // invalid input check #1
+            fprintf(stderr, "Error parsing line: %s", buffer);
+            return 1;
+        }
+
+        if (calories == 0) { // invalid input check #2
+            if (errno == EINVAL) {
+                fprintf(stderr, "Conversion error occurred: %d\n", errno);
+                return 1;
+            }
+        }
+
+        current_calories += calories; // add the calories to the current elf
     }
+
+    fclose(handle);
+
+    printf("Record calories: %d\n", record_calories);
     return 0;
 }

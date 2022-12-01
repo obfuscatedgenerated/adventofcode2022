8886ce31fee0a7d4bbd586e5fb3a8419efd46cab
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-12-01 10:43:40 +0000

    file reader

--- src/01/main.c
+++ src/01/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    printf("Hello, World!\n");
+    FILE* handle;
+    char* buffer;
+
+    handle = fopen("01.txt", "r");
+    if (handle == NULL) {
+        printf("Error opening file");
+        return 1;
+    }
+
+    fseek(handle, 0, SEEK_END);
+    size_t length = ftell(handle);
+    fseek(handle, 0, SEEK_SET);
+
+    buffer = malloc(length + 1);
+
+    if (buffer == NULL) {
+        printf("Error allocating memory");
+        return 1;
+    }
+
+    fread(buffer, 1, length, handle);
+    fclose(handle);
+
+    buffer[length] = '\0';
+
+    printf("%s", buffer);
     return 0;
 }
